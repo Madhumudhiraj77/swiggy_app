21aa5611ccfae6760a419c41660fe036523f4547
Madhu <madhumudhiraj77@gmail.com>
2025-03-07 15:54:32 +0530

    Updated vercel.json for proper build configuration

--- server/index.js
+++ server/index.js
@@ -27,8 +27,7 @@ app.get("/proxy", async (req, res) => {
       method: req.method,
       headers: {
         "Content-Type": "application/json",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",  // Add User-Agent
-        "Origin": "https://swiggy-app-phi.vercel.app"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"  // Add User-Agent
        },
     });
 
